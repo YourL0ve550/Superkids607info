f79423b3345a5bf82eb0cc549810cd9d64e288e9
YourL0ve550 <62862467+YourL0ve550@users.noreply.github.com>
2020-06-18 23:21:49 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">	<!--Idioma español-->
+	<link rel="icon" type="icon/png" href="https://vignette.wikia.nocookie.net/es.pokemon/images/2/2c/Pok%C3%A9mon_Jukebox_icono.png/revision/latest?cb=20150624132304">
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<title>Mi Primera Pagina Web</title>
+</head>
+<body>
+
+	<!--Barra de arriba-->
+	<nav id="Nav">
+		<h1>Bienvenidos</h1>
+	</nav>
+
+	<h1 class="Titulo">¿Que es la Programacion web?</h1>
+
+	<div class="Conte">
+		<p>
+			La programación de los sitios web es una de las disciplinas dentro del mundo de Internet que más se ha desarrollado y no deja de sorprender día a día con las posibilidades que abre y genera, ya que no sólo consigue satisfacer necesidades que se generan, sino que sin la generación de necesidades ofrecen servicios a los usuarios que éstos no habían imaginado.<br><br>
+
+			En principio, el gran desarrollo de Internet se fundamentó en la posibilidad de enlazar a través de hipervículos diferentes páginas web lo que generó la enorme interconexión que es hoy Internet. Esta base que inició esta red, fué desarrollada y generó el lenguaje ícono de las páginas web que es HTML.
+		</p>
+	</div>
+</body>
+</html>
